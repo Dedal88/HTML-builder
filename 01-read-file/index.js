b214fd6707162e0a5342783bfe5e428fac0c0f51
Dedal88 <audit37msv@yandex.ru>
2025-01-19 17:21:45 +0300

    feat: solution to a learning task 03-files-in-folder

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,9 @@
+// запрашиваем необходимые модули
 const fs = require('fs');
 const path = require('path');
+// получаем пути
 const dirPath = path.dirname(__filename);
 const pathTotal = path.join(dirPath, 'text.txt');
+// считываем файл и выводим результат
 const readStream = fs.createReadStream(pathTotal, 'utf-8');
 readStream.on('data', (chunk) => console.log(chunk));
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+// зпрашиваем необходимые модули
+const fs = require('fs');
+const path = require('path');
+const promis = require('fs/promises');
+// создаём пути, считываем файлы
+const dirPath = path.dirname(__filename);
+const pathTotal = path.join(dirPath, 'secret-folder');
+const files = promis.readdir(pathTotal, { withFileTypes: true });
+// обрабатываем промис с объектами считанных файлов и выводим результат
+files.then((data) => {
+  for (let i of data) {
+    const extFile = path.extname(i.name).slice(1);
+    if (i.isFile()) {
+      const pathFile = path.join(pathTotal, i.name);
+      const pathObj = path.parse(pathFile);
+      const fileName = pathObj.name;
+      const stats = promis.stat(pathFile);
+      stats.then((data) => {
+        console.log(`${fileName} - ${extFile} - ${data.size / 1000}kb`);
+      });
+    }
+  }
+});
